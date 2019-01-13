1b0183b13265724eed521f6919ef04277578c0e8
Alexander Forehand <alexander.forehand.5173@student.roguecc.edu>
2019-01-13 14:39:22 -0800

    Setup function prototypes

--- Lab1AlexForehand/Source.cpp
+++ Lab1AlexForehand/Source.cpp
@@ -11,7 +11,7 @@ Abstract: A menu driven database application that holds personal information.
 Inputs: employee_data.txt which will take in information entered by the user and store it.
 Keyboard input to navigate the menu.
 
-Output: data.txt; program will store information input by the user and display/modify when requested
+Output: employee_data.txt; program will store information input by the user and display/modify when requested
 using the menu provided by said program.
 **************************************************************************************/
 
@@ -30,6 +30,19 @@ using std::left;
 using std::right;
 using std::fixed;
 using std::showbase;
+
+const unsigned short NAME_LENGTH = 20;
+const unsigned short MAX_EMPLOYEES = 15;
+
+void DisplayMenu();
+void ReadFile(char fname[][NAME_LENGTH], char lname[][NAME_LENGTH], char phoneNum[][13], char dateOfBirth[][11], int numEmployees,
+    ofstream employeeOut, ifstream employeeIn); // read file into related arrays
+void SortUsers(char fname[][NAME_LENGTH], char lname[][NAME_LENGTH], char phoneNum[][13], char dateOfBirth[][11], int numEmployees, 
+    ofstream employeeOut); // sorts arrays by last name and then writes changes back to data file
+void MenuChoice(char fname[][NAME_LENGTH], char lname[][NAME_LENGTH], char phoneNum[][13], char dateOfBirth[][11],
+    int numEmployees, ofstream employeeOut, ifstream employeeIn); // processes user menu choice
+void EditUser(char fname[][NAME_LENGTH], char lname[][NAME_LENGTH], char phoneNum[][13], char dateOfBirth[][11]); // Allows user to edit a specific user
+void PrintUsers(char fname[][NAME_LENGTH], char lname[][NAME_LENGTH], char phoneNum[][13], char dateOfBirth[][11], int numEmployees);
 
 int main() {
 
