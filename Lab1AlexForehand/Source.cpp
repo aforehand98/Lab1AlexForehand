3ffdd4e19401da84cf6a078511d615b239f0ae5a
Alexander Forehand <alexander.forehand.5173@student.roguecc.edu>
2019-01-13 13:34:42 -0800

    Add project files.

---
+++ Lab1AlexForehand/Source.cpp
@@ -0,0 +1,36 @@
+/*************************************************************************************
+Programmer: Alex Forehand
+Organization: Rogue Community College - CS233
+Filename: main.cpp
+Date: JAN92019 10:00 PM
+Modifications:
+
+Lab #: 1
+Abstract: A menu driven database application that holds personal information.
+
+Inputs: employee_data.txt which will take in information entered by the user and store it.
+Keyboard input to navigate the menu.
+
+Output: data.txt; program will store information input by the user and display/modify when requested
+using the menu provided by said program.
+**************************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::ofstream;
+using std::ifstream;
+using std::setprecision;
+using std::setw;
+using std::endl;
+using std::left;
+using std::right;
+using std::fixed;
+using std::showbase;
+
+int main() {
+
+}
